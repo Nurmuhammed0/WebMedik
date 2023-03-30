4465802d743e301b47bbb7993836c539f227e4c9
Nurmuhammed0 <Nur26kudayarov@gmail.com>
2023-03-30 15:17:24 +0600

    ozgordu

--- src/WebMedik/menu/LargeCards/Stars1.js
+++ src/WebMedik/menu/LargeCards/Stars1.js
@@ -8,7 +8,7 @@ export default function Stars1() {
   return (
     <div style={{alignItems:"center"}} spacing={1}>
       {/* <Rating name="half-rating" defaultValue={3} precision={0.5} activeColor="red"/> */}
-      j
+      
       <Rating name="half-rating-read" defaultValue={2} precision={0.5} readOnly />
     </div>
   );
